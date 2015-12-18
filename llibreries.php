cf80e2830f61ee4c95e50fad8ed108844d13f428
Roger Freixes <rogercat23@gmail.com>
2015-12-18 12:54:50 +0100

    Pas 6
    Frameword afegit protoshow les llibreries (js i css) i he fciat imatges
    de demo també per mirar si funciona. Funciona encara tingui error
    important load al arxiu scripts.js.

--- llibreries.php
+++ llibreries.php
@@ -3,6 +3,7 @@
 <link href="css/pnotify.custom.min.css" rel="stylesheet">
 <link rel="stylesheet" href="css/bootstrap-theme.css">
 <link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/protoshow.css">
 <link href="css/estil.css" rel="stylesheet">
 
 <!-- LLIBRERIES DE LA PÀGINA -->
@@ -10,4 +11,8 @@
 <script src="lib/jquery-ui.js"></script>
 <script src="lib/pnotify.custom.min.js"></script>
 <script src="lib/bootstrap.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/scriptaculous/1.8.3/scriptaculous.js"></script>
+<script src="scripts/protoshow.js"></script>
+<script src="scripts/site.js"></script>
 <script src="scripts/scripts.js"></script>
